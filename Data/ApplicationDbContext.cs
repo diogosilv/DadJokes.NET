81178f773ba2055eb8c0d32f789a6ddbf18ed9e0
diogosilv <diogoabelsilva@hotmail.com>
2024-01-18 17:48:31 +0000

    Dad Jokes Project .NET

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DadJokesWebApp.Models;
+
+namespace DadJokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DadJokesWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
